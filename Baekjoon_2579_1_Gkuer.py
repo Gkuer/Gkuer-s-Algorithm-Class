a36623f834b29cd2db4742b6fb67753fdf0033b9
gkuer <gkuer95@gmail.com>
2021-11-28 23:39:18 +0900

    upload-solve

---
+++ Baekjoon_2579_1_Gkuer.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [0] * 301
+for i in range(n):
+    arr[i] = int(input())
+
+maps = [0] * 301
+maps[0] = arr[0]
+maps[1] = arr[0]+arr[1]
+maps[2] = max(arr[0]+arr[2], arr[1]+arr[2])
+for i in range(3,n):
+    maps[i] = max(maps[i-2]+arr[i], maps[i-3]+arr[i-1]+arr[i])
+
+print(maps[n-1])
+# Pass
