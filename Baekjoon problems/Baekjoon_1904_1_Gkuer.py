87c9c1ee265a5b140be088daad081d8a0e0c57ad
gkuer <gkuer95@gmail.com>
2021-12-06 12:50:32 +0900

    upload-solve

---
+++ Baekjoon problems/Baekjoon_1904_1_Gkuer.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [0] * (n+1)
+arr[0] = 1
+arr[1] = 2
+for i in range(2,n+1):
+    arr[i] = (arr[i-2] + arr[i-1]) % 15746
+
+print(arr[n-1] % 15746)
+
+# Pass
