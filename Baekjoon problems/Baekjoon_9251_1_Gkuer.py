169998b393bdecc7bc404af4c7f54f76b4d37b9e
gkuer <gkuer95@gmail.com>
2021-12-07 22:48:46 +0900

    upload-solve

---
+++ Baekjoon problems/Baekjoon_9251_1_Gkuer.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int,input().split())
+coins = []
+for _ in range(n):
+    coins.append(int(input()))
+dp = [0] * (k+1)
+dp[0] = 1
+for i in coins:
+    for j in range(1,k+1):
+        if j - i >= 0:
+            dp[j] += dp[j-i]
+print(dp[k])
+
+# Pass
