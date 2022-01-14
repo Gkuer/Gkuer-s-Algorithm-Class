0d6753ca16b083ab2e2af48561794f1f8eca9d54
gkuer <gkuer95@gmail.com>
2022-01-14 12:05:16 +0900

    upload-solve

---
+++ Baekjoon problems Python/Baekjoon_15650_1_Gkuer.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def perm(start):
+    if len(maps) == m:
+        print(*maps)
+        return
+
+    for i in range(start,n+1):
+        if i not in maps:
+            maps.append(i)
+            perm(i+1)
+            maps.pop()
+
+n, m = map(int,input().split())
+maps = []
+perm(1)
