5fe30bfecba234cb32403a0a67c4cf7acf34681d
gkuer <gkuer95@gmail.com>
2022-02-17 10:53:13 +0900

    upload-solve

--- Backjoon problems Java/Baekjoon_1343_1_Gkuer.java
+++ Backjoon problems Java/Baekjoon_1343_1_Gkuer.java
@@ -1,16 +1,28 @@
 import java.util.Scanner;
+
 public class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int n1 = sc.nextInt();
-		int n2 = sc.nextInt();
-		sc.close();
-		
-		int ans = n1 - n2;
-		System.out.println(ans);
-	}
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String maps = sc.next();
+        maps = maps.replace("XXXX", "AAAA");
+        maps = maps.replace("XX", "BB");
+
+        Boolean flag = false;
+        for (int i = 0; i < maps.length(); i++) {
+            if (maps.charAt(i) == 'X') {
+                flag = true;
+            }
+        }
+
+        if (flag) {
+            System.out.println(-1);
+        } else {
+            System.out.println(maps);
+        }
+    }
 
 }
 
