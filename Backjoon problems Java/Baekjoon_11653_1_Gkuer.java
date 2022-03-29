0991aa1de0f3e9b57b37f3cfb14aa05a5c268589
gkuer <gkuer95@gmail.com>
2022-03-30 00:00:40 +0900

    FEAT: Solve

--- Backjoon problems Java/Baekjoon_11653_1_Gkuer.java
+++ Backjoon problems Java/Baekjoon_11653_1_Gkuer.java
@@ -1,64 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        Boolean[][] maps = new Boolean[n][m];
+        int n = Integer.parseInt(br.readLine());
+        int s = 2;
 
-        for (int i=0; i<n; i++) {
-            String data = sc.next();
-            for (int j=0; j<m; j++) {
-                if (data.charAt(j) == 'W') {
-                    maps[i][j] = true;
-                } else {
-                    maps[i][j] = false;
-                }
+        while (n > 1) {
+            if (n % s == 0) {
+                n /= s;
+                bw.write(s + "\n");
+            } else {
+                s += 1;
             }
         }
 
-        int ans = 2500;
-        for (int i=0; i<n-7; i++) {
-            for (int j=0; j<m-7; j++) {
-                int w_cnt = 0;
-                int b_cnt = 0;
-
-                for (int k=0; k<8; k++) {
-                    for (int l=0; l<8; l++) {
-                        if ((k+l) % 2 == 0) {
-                            if (maps[i+k][j+l]) {
-                                w_cnt += 1;
-                            }
-                            if (!maps[i+k][j+l]) {
-                                b_cnt += 1;
-                            }
-                        } else {
-                            if (maps[i+k][j+l]) {
-                                b_cnt += 1;
-                            }
-                            if (!maps[i+k][j+l]) {
-                                w_cnt += 1;
-                            }
-                        }
-                    }
-                }
-
-                if (ans > w_cnt) {
-                    ans = w_cnt;
-                }
-                if (ans > b_cnt) {
-                    ans = b_cnt;
-                }
-            }
-        }
-
-        System.out.println(ans);
+        bw.flush();
     }
-
 }
 
 // Pass
--- Backjoon problems Java/Baekjoon_11723_1_Gkuer.java
+++ Backjoon problems Java/Baekjoon_11723_1_Gkuer.java
@@ -1,17 +1,65 @@
-import java.util.Scanner;
-public class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int n1 = sc.nextInt();
-		int n2 = sc.nextInt();
-		sc.close();
-		
-		int ans = n1 - n2;
-		System.out.println(ans);
-	}
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Array;
+import java.util.*;
 
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<String> maps = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String program;
+            String data;
+
+            String command = br.readLine();
+            if (!(command.equals("all") || (command.equals("empty")))) {
+                String[] listCommand = command.split(" ");
+                program = listCommand[0];
+                data = listCommand[1];
+
+                if (program.equals("add")) {
+                    if (!maps.contains(data)) {
+                        maps.add(data);
+                    }
+                } else if (program.equals("remove")) {
+                    if (maps.contains(data)) {
+                        maps.remove(data);
+                    }
+                } else if (program.equals("check")) {
+                    if (maps.contains(data)) {
+                        bw.write(1 + "\n");
+                    } else {
+                        bw.write(0 + "\n");
+                    }
+                } else if (program.equals("toggle")) {
+                    if (maps.contains(data)) {
+                        maps.remove(data);
+                    } else {
+                        maps.add(data);
+                    }
+                }
+            } else {
+                if (command.equals("all")) {
+                    maps = new ArrayList<>();
+                    for (int j = 1; j < 21; j++) {
+                        maps.add(String.valueOf(j));
+                    }
+                } else {
+                    maps = new ArrayList<>();
+                }
+            }
+        }
+
+        bw.flush();
+    }
 }
 
 // Pass
