7cc562763c53d85dcebfb86b61e3c2567befefdb
gkuer <gkuer95@gmail.com>
2022-04-12 19:40:43 +0900

    FEAT: Solve

--- Baekjoon problems Python 2/Baekjoon_TEST_2_Gkuer.py
+++ Baekjoon problems Python 2/Baekjoon_TEST_2_Gkuer.py
@@ -1,22 +1,55 @@
-# ([i for i in range(128)] (평균) * 10) + ([i for i in range(128) (평균) * 5)
-# --------------------------------------------------------------------------   === [i for i in range(128)], 15 [새로운 평균값(?)]
-#                                  각 인자 나누기 15
+from collections import deque
+
+dirs = [(0,1),(0,-1)]
+dirs_x = [(1,0),(-1,0)]
+
+def find(dense):
+    temps = [[0]*m for _ in range(n)]
+    temps[start_x][start_y] = 1
+    q = deque()
+    q.append([start_x,start_y])
+    while q:
+        x, y = q.popleft()
+        for i in range(2):
+            nr = x + dirs[i][0]
+            nc = y + dirs[i][1]
+            if 0 <= nr < n and 0 <= nc < m:
+                if not temps[nr][nc]:
+                    temps[nr][nc] = 1
+                    if maps[nr][nc] == 1:
+                        q.append([nr,nc])
+                    elif maps[nr][nc] == 3:
+                        return True
+
+        for j in range(1,dense+1):
+            for i in range(2):
+                nr = x + dirs_x[i][0]*j
+                nc = y + dirs_x[i][1]*j
+                if 0 <= nr < n and 0 <= nc < m:
+                    if not temps[nr][nc]:
+                        temps[nr][nc] = 1
+                        if maps[nr][nc] == 1:
+                            q.append([nr,nc])
+                        elif maps[nr][nc] == 3:
+                            return True
+
+    return False
 
 
-a = [2,3,5,7]
-b = [5,5,2,1]
-ans = []
+t = int(input())
+for tc in range(1, t+1):
+    n, m = map(int,input().split())
+    maps = [list(map(int,input().split())) for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] == 2:
+                start_x, start_y = i, j
 
-a_cnt = 10sd
-b_cnt = 5
+    step = 0
+    while True:
+        res = find(step)
+        if res:
+            break
+        step += 1
 
-for i in range(len(a)):
-    a[i] *= a_cnt
-
-for i in range(len(b)):
-    b[i] *= b_cnt
-
-for i in range(4):
-    ans.append((a[i]+b[i])/(a_cnt+b_cnt))
-
-print(str([1,2,3,4]))
+    print("#{} {}".format(tc, step))
